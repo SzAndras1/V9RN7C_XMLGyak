ced9ed8913bd1102d2138e06e8bd83389cfc2088
SzAndras1 <szbandras2@gmail.com>
2022-12-04 23:53:47 +0100

    Update lesson work

--- V9RN7C_1130/JSONParseV9RN7C/src/main/v9rn7c/JSONWriteV9RN7C.java
+++ V9RN7C_1130/JSONParseV9RN7C/src/main/v9rn7c/JSONWriteV9RN7C.java
@@ -1,7 +1,22 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JSONWriteV9RN7C {
-    public static void main(String[] args) {
-        JSONParser parser = new JSONParser();
+    public static void main(String[] args) throws IOException {
+        FileWriter file = new FileWriter("JsoninTxt.txt");
+        JSONArray array = new JSONArray();
+        List content = new ArrayList();
+        content.add(new String("Bl"));
+        content.add(1000000.0);
+        content.add(21);
+        array.add(content);
+        file.write(array.toJSONString());
+        file.close();
     }
 }
